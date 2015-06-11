204ae3aa8d601f5a95b407d9386598c4e8f6d309
Afoucaul <armand.foucault@telecom-bretagne.eu>
2015-06-11 14:00:21 +0200

    lol

--- InWordKanji.py
+++ InWordKanji.py
@@ -1,3 +1,7 @@
 ﻿# Author : Armand FOUCAULT
 
 class InWordKanji(Kanji) :
+	def __init__(self, character, meanings=[], strokes=0, position=0) :
+		Kanji.__init__(character, meanings, strokes)
+		# an int that represents the position of the kanji in 
+		self.position = position
